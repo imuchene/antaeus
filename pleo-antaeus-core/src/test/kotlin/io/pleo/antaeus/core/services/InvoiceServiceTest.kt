97e64d6d44ee9320617ec9772211f9c6df096a45
Isaiah Muchene <imuchene@msn.com>
2021-09-30 16:52:08 +0300

    Create a unit test that tests if an invoice is pending.

--- pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
+++ pleo-antaeus-core/src/test/kotlin/io/pleo/antaeus/core/services/InvoiceServiceTest.kt
@@ -6,10 +6,27 @@ import io.pleo.antaeus.core.exceptions.InvoiceNotFoundException
 import io.pleo.antaeus.data.AntaeusDal
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.Money
+import io.pleo.antaeus.models.Currency
+import io.pleo.antaeus.models.InvoiceStatus
 
 class InvoiceServiceTest {
+
+    private val mockInvoice = Invoice(
+        id = 1, 
+        customerId = 500, 
+        amount = Money(
+            value = 1000.toBigDecimal(), 
+            currency = Currency.EUR
+            ), 
+        status = InvoiceStatus.PENDING
+        )
+
+        
     private val dal = mockk<AntaeusDal> {
         every { fetchInvoice(404) } returns null
+        every { isInvoicePending(mockInvoice.id) } returns mockInvoice
     }
 
     private val invoiceService = InvoiceService(dal = dal)
@@ -18,6 +35,13 @@ class InvoiceServiceTest {
     fun `will throw if invoice is not found`() {
         assertThrows<InvoiceNotFoundException> {
             invoiceService.fetch(404)
+        }
+    }
+
+    @Test
+    fun `will return true if the invoice is pending`() {
+        assert(true) {
+            invoiceService.isInvoicePending(mockInvoice.id) as Invoice
         }
     }
 }
